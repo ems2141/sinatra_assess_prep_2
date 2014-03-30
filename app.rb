aaf677e24b67768e12732d48745e8012b34adddb
Ellie Schneiders <ems2141@gmail.com>
2014-03-30 13:46:54 -0600

    user can create a new task.

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 require 'sinatra/base'
 
 class App<Sinatra::Application
+
+  NEW_TASK = ""
+
   get '/' do
     erb :index
   end
+
+  get '/new' do
+    erb :new
+  end
+
+  post '/' do
+    NEW_TASK = params['new_task']
+    erb :index
+  end
+
 end
